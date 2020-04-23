55cd84af175c3a6015dc710b8c3dcd4e5e8d70b0
ESTEBAN CANDO <esteban.cando@trade.ec>
2020-04-23 16:55:32 -0500

    agregar base dialogflow

--- controller/webhook.js
+++ controller/webhook.js
@@ -1,13 +1,20 @@
- //const employees = require('../db_apis/employees.js');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
  
 async function post(req, res, next) {
   try {
-    const context = {};
- 
-    const rows = "webhook";
- 
-
-    res.status(200).json(rows);
+      const agent = new WebhookClient({ req, res });
+      console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+      console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+     
+      function welcome(agent) {
+        agent.add(`Welcome from webhook`);
+      }
+     
+      function fallback(agent) {
+        agent.add(`I didn't understand from webhook`);
+        agent.add(`I'm sorry, can you try again? from webhook`);
+      }
 
   } catch (err) {
     next(err);
