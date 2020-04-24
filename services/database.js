820988da60ddb19a3f1fc5c2fda324157e224062
ESTEBAN CANDO <esteban.cando@trade.ec>
2020-04-23 19:16:22 -0500

    eliminar oracle

--- services/database.js
+++ services/database.js
@@ -1,25 +1,25 @@
-const oracledb = require('oracledb');
+//const oracledb = require('oracledb');
 const dbConfig = require('../config/database.js');
  
 async function initialize() {
-  const pool = await oracledb.createPool(dbConfig.hrPool);
+  //const pool = await oracledb.createPool(dbConfig.hrPool);
 }
 
 async function close() {
-    await oracledb.getPool().close();
+    //await oracledb.getPool().close();
   }
 
 function simpleExecute(statement, binds = [], opts = {}) {
     return new Promise(async (resolve, reject) => {
       let conn;
    
-      opts.outFormat = oracledb.OBJECT;
+      //opts.outFormat = oracledb.OBJECT;
       opts.autoCommit = true;
    
       try {
-        conn = await oracledb.getConnection();
+        //conn = await oracledb.getConnection();
    
-        const result = await conn.execute(statement, binds, opts);
+        //const result = await conn.execute(statement, binds, opts);
    
         resolve(result);
       } catch (err) {
